bb0f292e1111e4c19d6fd7284deaf2396e630457
luke afolabi <admin@asynclearning.io>
2021-03-20 09:17:12 -0700

    FlamingoApp - Firebase context setup

---
+++ src/context/firebaseContext.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSEHh9WZ_YC3INeQsG2Tqm5iv9YpiBMIA",
+  authDomain: "asynclearning-e216e.firebaseapp.com",
+  databaseURL: "https://asynclearning-e216e.firebaseio.com",
+  projectId: "asynclearning-e216e",
+  storageBucket: "asynclearning-e216e.appspot.com",
+  messagingSenderId: "665630737781",
+  appId: "1:665630737781:web:6e699c3642fa1fbf343f3c",
+};
